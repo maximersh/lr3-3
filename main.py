59418b22fe15c3b0e20a5dc6b93f765e1fe5680e
Максим <maximers6e@gmail.com>
2024-10-03 09:44:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-day = int(input("Введите день: "))
-month = int(input("Введите месяц: "))
-if (month == 3 and day >= 1) or (month == 4) or (month == 5) or (month == 6 and day <= 31):
-    season = "Весна"
-elif (month == 6 and day >= 1) or (month == 7) or (month == 8) or (month == 8 and day <= 31):
+Ввод данных от пользователя
+day = int(input("Введите день: ")) #Ввод дня
+month = int(input("Введите месяц: ")) #Ввод месяца
+#определение сезона
+if (month == 3 and day >= 1 and day <=31) or (month == 4 and day >= 1 and day <=30) or (month == 5 and day >= 1 and day <=31) :
+    season = "Весна" 
+elif (month == 6 and day >= 1 and day <=30) or (month == 7 and day >= 1 and day <=31) or (month == 8 and day >= 1 and day <=31) :
     season = "Лето"
-elif (month == 9 and day >= 1) or (month == 10) or (month == 11 and day <= 30):
+elif (month == 9 and day >= 1 and day <=30) or (month == 10 and day >= 1 and day <=31 ) or (month == 11 and day >=1 and day <=30 ):
     season = "Осень"
-else:
+elif (month == 1 and day >= 1 and day <=31) or (month == 2 and day >= 1 and day <=29 ) or (month == 12 and day >=1 and day <=31 ):
     season = "Зима"
-print(f"Дата: {day}.{month} относится к сезону: {season}")
+else: 
+    season=" ERROR: Введены неккоректные данные"
+# вывод сезона
+print("Время года:", season)
