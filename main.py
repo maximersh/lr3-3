b9aabd9b911999b5c17f4613f3cbf633ab2873e2
Максим <maximers6e@gmail.com>
2024-10-03 09:57:37 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 Ввод данных от пользователя
 day = int(input("Введите день: ")) #Ввод дня
 month = int(input("Введите месяц: ")) #Ввод месяца
+
 #определение сезона
 if (month == 3 and day >= 1 and day <=31) or (month == 4 and day >= 1 and day <=30) or (month == 5 and day >= 1 and day <=31) :
     season = "Весна" 
@@ -11,6 +12,7 @@ elif (month == 9 and day >= 1 and day <=30) or (month == 10 and day >= 1 and day
 elif (month == 1 and day >= 1 and day <=31) or (month == 2 and day >= 1 and day <=29 ) or (month == 12 and day >=1 and day <=31 ):
     season = "Зима"
 else: 
-    season=" ERROR: Введены неккоректные данные"
+    season=" ERROR: Введены неккоректные данные" # если неверные значения
+    
 # вывод сезона
 print("Время года:", season)
